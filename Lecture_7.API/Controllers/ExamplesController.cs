d742879c34a228dff477fceef42055c2e757af75
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-12-06 18:38:52 +0300

    Lecture_7_1

---
+++ Lecture_7.API/Controllers/ExamplesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture_7.API.Controllers
+{
+    [Route("api/[controller]")]  //route dediğimiz ne ? ==> url'e yazdığımız ve o endpointe gittiğimiz yerler oluyor. localhost:5611/api/contollerinadı yani bizimki examples =>  localhost/api/examples 
+    [ApiController]
+    public class ExamplesController : ControllerBase  
+    {
+        // burası sadece bir sınıf, içine aksiyon(işlem) eklemem gerek.
+        [HttpGet("[action]")]
+        public string TrimText(string text)  //bu bir metot!!!! METOTLAR SADECE CLASSLARIN İÇİNE YAZILABİLİR --AYRICA fonksiyonlar public private gibi erişim belirleyiciler almazlar. --Fonksiyonlar da metotlar da büyük harfle başlamalıdır !!!!!!!!!!!!!
+        {  //
+            return text.Trim();
+        }
+
+        //query olanda query yazar, route olanda path yazar   !!
+        [HttpGet("[action]")]
+        public string UpperText([FromBody]string text)  //fromroute yazınca url'de açık açık gözüküyor ama frombody yazınca gözükmüyor. Onu gizliyor örneğin password yazsak açık açık görünür from route yazınca ama bu frombody yazınca gizleniyor. Fakat eğer frombody kullanacaksak httpget kullanılmaz, httppost kullanabiliriz.
+        { 
+            return text.ToUpper();
+        }
+    }
+}
